a636234d87fd477996fe7d85b2eb36a21250cd4b
Josh Fry <jfry@mbayaq.org>
2013-04-10 14:28:32 -0700

    converted to spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,25 @@
 module.exports = function (grunt) {
-	grunt.initConfig({
-		uglify: {
-			dist: {
-				files: {
-					'js/app.min.js': ['js/plugins/*.js', 'js/app.js']
-				},
-				options: {
-					banner: '/* Built on <%= grunt.template.today() %> */ '
-				}
-			}
-		},
-		watch: {
-			files: ['js/plugins/*.js', 'js/app.js'],
-			tasks: ['uglify:dist']
-		}
-	});
+  grunt.initConfig({
+    uglify: {
+      dist: {
+        files: {
+          'js/app.min.js': ['js/plugins/*.js', 'js/app.js']
+        },
+        options: {
+          banner: '/* Built on <%= grunt.template.today() %> */ '
+        }
+      }
+    },
+    watch: {
+      files: ['js/plugins/*.js', 'js/app.js'],
+      tasks: ['uglify:dist']
+    }
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', [
-		'uglify'
-	]);
+  grunt.registerTask('default', [
+    'uglify'
+  ]);
 };
